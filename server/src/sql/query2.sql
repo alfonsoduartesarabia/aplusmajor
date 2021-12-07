cb5719204be401170ad7c6af5fbbc0dc1efcf178
Battulga Tsogtgerel <togasundays@gmail.com>
2021-12-06 20:02:02 -0800

    TOGA changes

--- server/src/sql/query2.sql
+++ server/src/sql/query2.sql
@@ -1,6 +1,10 @@
 INSERT INTO songs (id, song_title, notes) 
 VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
+INSERT INTO songs (id, song_title, notes) 
 VALUES (2, 'Up n coming Artist Tulga', 'C4 C4 D4 E4 E4 D4 D4 E4 E4 F4 G4 G4 F4 E4 D4 ');
+INSERT INTO songs (id, song_title, notes) 
 VALUES (3, 'Halsey', 'D4 C4 C4 D4 E4 E4 D4 D4 E4 E4 F4 G4 G4 F4 E4');
+INSERT INTO songs (id, song_title, notes) 
 VALUES (4, 'Snoop Dogg', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
+INSERT INTO songs (id, song_title, notes) 
 VALUES (5, 'Unknown Artist', 'F4 E4 D4 C4 C4 E4 E4 F4 G4 G4 D4 E4 E4 D4 D4');
